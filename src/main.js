c9fefff6bbe3d0d66ff0d33f9b81eb445e83d231
uiltrakeypoint <uiltrakeypoint@gmail.com>
2023-03-08 16:23:24 +0900

    first version

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import { createApp } from "vue";
-import VueDayjs from 'vue3-dayjs-plugin'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import {createApp} from "vue";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import Unicon from "vue-unicons";
+import {uniDesktop, uniPizzaSlice, uniGithub} from "vue-unicons/dist/icons";
 import "./style.scss";
+
+import BubbleEffect from "./components/BubbleEffect/BubbleEffect.vue";
 import App from "./App.vue";
 
+import * as db from "./utils/db";
+
+await db.initDatabase();
+
 const app = createApp(App);
+app.component("BubbleEffect", BubbleEffect);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+Unicon.add([uniDesktop, uniPizzaSlice, uniGithub]);
 
 app.use(ElementPlus);
-app.use(VueDayjs);
-app.mount('#app');
+app.use(Unicon);
+app.mount("#app");
