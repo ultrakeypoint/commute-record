f2202ec77c9e6ef5378abb69c6bcb89c9f30d3bc
ultrakeypoint <ultrakeypoint@gmail.com>
2023-03-14 22:58:47 +0900

    Add 0.0.2

---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import ko from "./ko";
+import en from "./en";
+
+export default {
+  ko,
+  en,
+};
--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import {createApp} from "vue";
+import {createI18n} from "vue-i18n";
+import langs from "./i18n";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import ElementPlus from "element-plus";
+import ElementKo from "element-plus/dist/locale/ko.min.mjs";
 import "element-plus/dist/index.css";
 import Unicon from "vue-unicons";
-import {uniDesktop, uniPizzaSlice, uniGithub} from "vue-unicons/dist/icons";
+import {
+  uniDesktop,
+  uniPizzaSlice,
+  uniGithub,
+  uniSearch,
+  uniPlus,
+  uniFileDownload,
+  uniTrash,
+} from "vue-unicons/dist/icons";
 import "./style.scss";
-
 import BubbleEffect from "./components/BubbleEffect/BubbleEffect.vue";
 import App from "./App.vue";
-
 import * as db from "./utils/db";
+
+const i18n = createI18n({
+  locale: "ko",
+  fallbackLocale: "en",
+  messages: langs,
+});
 
 await db.initDatabase();
 
@@ -20,8 +35,19 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-Unicon.add([uniDesktop, uniPizzaSlice, uniGithub]);
+Unicon.add([
+  uniDesktop,
+  uniPizzaSlice,
+  uniGithub,
+  uniSearch,
+  uniPlus,
+  uniFileDownload,
+  uniTrash,
+]);
 
-app.use(ElementPlus);
+app.use(i18n);
+app.use(ElementPlus, {
+  locale: ElementKo,
+});
 app.use(Unicon);
 app.mount("#app");
